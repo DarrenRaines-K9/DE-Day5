0b27d0cd303bd01c135209d1cc233d22e9c81ad8
Darren Raines <draines.123123@gmail.com>
2025-10-28 06:38:15 -0500

    csv to postgress kinda

--- src/main.py
+++ src/main.py
@@ -1,27 +1,48 @@
 import csv
 import psycopg2
+import logging
 
 
 # Get logger
 def get_logger():
-    pass
+    logger = logging.getLogger("my_logger")
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
 
 
 # Read CSV file
-def process_csv():
+def process_csv(logger):
     file_path = "data/sample.csv"
     mode = "r"
+    number_of_rows = 0
+    insert_query = """
+INSERT INTO people (id, name, age, city)
+VALUES (%s, %s, %s, %s)
+"""
 
     conn = postgres_connection()
     cursor = conn.cursor()
+
+    logger.info("reading file...")
     with open(file_path, mode) as csv_file:
         csv_reader = csv.reader(csv_file)
-
+        number_of_rows += 1
         next(csv_reader)  # Skip header row
 
         for row in csv_reader:
-            print(row)
 
+            cursor.execute(insert_query, row)
+
+    logger.info(f"Number of rows processed: {number_of_rows}")
+    conn.commit()
     cursor.close()
     conn.close()
 
@@ -39,7 +60,10 @@ def postgres_connection():
 
 # For basic Main setup
 def main():
-    process_csv()
+    logger = get_logger()
+    logger.info("Starting CSV processing...")
+    process_csv(logger)
+    logger.info("CSV processing completed.")
 
 
 if __name__ == "__main__":
